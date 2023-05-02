2e3cfe684883a0a836a91bff64dd7e482d674483
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-05-02 21:24:33 +0200

    media

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,10 +4,12 @@ import "controllers"
 
 
 const nav_shows = document.getElementById('nav-shows')
+const shows = document.getElementById('upcoming-shows')
 const nav_about = document.getElementById('nav-about')
 const about = document.getElementById('about')
-const nav_listen = document.getElementById('nav-listen')
 const nav_shop = document.getElementById('nav-shop')
 
-console.log(about.offsetTop)
-nav_about.addEventListener('click', () => { window.scrollTo(0, about.offsetTop - 80) })
+nav_about.addEventListener('click', () => { window.scrollTo({top: (about.offsetTop - 80), behavior: "smooth" }) })
+nav_shows.addEventListener('click', () => { window.scrollTo({top: (shows.offsetTop - 80), behavior: "smooth" }) })
+// nav_shows.addEventListener('click', () => { window.scrollTo(0, shows.offsetTop - 80) })
+// nav_about.addEventListener('click', () => { window.scrollTo(0, .offsetTop - 80) })
