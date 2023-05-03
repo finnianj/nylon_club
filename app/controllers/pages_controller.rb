aa087d83795d4bebd7329afd2dbcdd9afb1d0117
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-05-03 11:48:57 +0200

    pdelete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,12 @@ class PagesController < ApplicationController
   end
 
   def admin
-    @past_shows = Show.where("date < ?", Date.today).order('date DESC')
-    @upcoming_shows = Show.where("date >= ?", Date.today).order('date ASC')
+    @shows = Show.all
+  end
+
+  def destroy_show
+    show = Show.find(params[:show])
+    raise
+    redirect_to admin_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  get '/welcometozelda/bitch', to: 'pages#admin'
+  get '/welcometozelda/bitch', to: 'pages#admin', as: 'admin'
+
+  delete '/:show', to: 'pages#destroy_show', as: 'delete'
+
 
   # Defines the root path route ("/")
   # root "articles#index"
